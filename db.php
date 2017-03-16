fb833b70ffb7377dd12e0ea97076efcb34b0bf6d
jorgenvk <jorgenvk@hotmail.com>
2017-03-16 13:25:51 +0100

    Revert "db.php connect, create db, create table //test"
    This reverts commit 020c4b983830c7cab8abd55a3f2417cc9fd5c05c.

--- db.php
+++ db.php
@@ -2,39 +2,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "testdb";
 
-// Kobler til
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn= new mysqli($servername, $username, $password);
 
-// Sjekker kobling
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Connected successfully<br>";
-
-// Oppretter database
-$sql = "CREATE DATABASE testdb";
-if ($conn->query($sql) === true) {
-    echo "Opprettet database <br>";
-} else {
-    echo "Database ble ikke opprettet: " . $conn->error . "<br>";
-}
-
-// Lager en tabell i databasen
-
-$sql = "CREATE TABLE tabelltest (
-id INT(6) AUTO_INCREMENT PRIMARY KEY,
-navn VARCHAR(32) NOT NULL
-)";
-
-
-
-if ($conn->query($sql) === TRUE) {
-    echo "Tabell testtabell ble opprettet <br>";
-} else {
-    echo "Tabell ble ikke opprettet: " . $conn->error . "<br>";
-}
-
-$conn->close();
+echo "Connected successfully";
