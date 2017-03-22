e201f1eb0ad925255a570bf5a570e5ab841e0945
thyal <thomasdfrivold@gmail.com>
2017-03-22 16:31:53 +0100

    La til table i resultatet av spørringen, så det blir mer oversiktelig

--- php/list.php
+++ php/list.php
@@ -19,9 +19,11 @@ $result = $conn->query($sql);
 
 //Hvis resultatet av spørringen gir mer enn null rader, skriv ut. 
 if($result->num_rows > 0) {
+	echo "<table><tr><th>Bedriftens navn</th><th>Kategori</th><th>Adresse</th><th>Tlf</th><th>Beskrivelse</th><th>Åpningstider</th><th>Nettside</th></tr>";
 	while($row = $result->fetch_assoc()) {
-		echo "Bedriftnavn: " . $row["Bedrift_navn"]. " Kategori: " . $row["Kategori_navn"]. " Adresse: ". $row["Adresse"]. " Tlf: " . $row["Telefon"]. " Beskrivelse: ". $row["Beskrivelse"]. " Åpningstider: ". $row["Åpningstider"]. " Nettside: " . $row["Nettside"]. "<br>";
+		echo "<tr><td>".$row["Bedrift_navn"]."</td><td>".$row["Kategori_navn"]."</td><td>".$row["Adresse"]."</td><td>".$row["Telefon"]. "</td><td>".$row["Beskrivelse"]."</td><td>".$row["Åpningstider"]."</td><td>".$row["Nettside"]."</td></tr>";
 	}
+	echo "</table>";
 }	else  { 
 	echo "ikke noe resultat";
 
