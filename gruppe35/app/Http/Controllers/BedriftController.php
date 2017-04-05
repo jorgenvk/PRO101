de443c71c750e623f23f228ebd02161e0973af87
Joakim Vrangum <joakimvrangum@gmail.com>
2017-04-05 14:33:43 +0200

    Fikset opplasting av profilbilde til Bedrift

--- gruppe35/app/Http/Controllers/BedriftController.php
+++ gruppe35/app/Http/Controllers/BedriftController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Bedrift;
 use App\Kategori;
+use Storage;
 
 class BedriftController extends Controller
 {
@@ -31,11 +32,10 @@ class BedriftController extends Controller
 
     public function postNyBedrift(Request $request)
     {
-    	// TO-DO legge til Validator, sørge for at form-data er gyldig, osv..
-
-        if ($request->hasFile('fil-profilbilde'))
+    	// TO-DO legge til Validator, sørge for at form-data er gyldig, osv.
+        if ($request->hasFile('fil'))
             {
-                if ($s3upload = Storage::disk('s3')->putFile('bedrift-profil', $request->file('fil-profilbilde'), 'public'))
+                if ($s3upload = Storage::disk('s3')->putFile('', $request->file('fil'), 'public'))
                     {
                         // Profilbildet *ble* laset opp, opprett bedrift;
                         $bedrift = New Bedrift;
@@ -46,13 +46,13 @@ class BedriftController extends Controller
                         $bedrift->Beskrivelse   = $request->Beskrivelse;
                         $bedrift->Åpningstider  = $request->Åpningstider;
                         $bedrift->Nettside      = $request->Nettside;
-                        $bedrift->Bilde         = $s3upload;
+                        $bedrift->Bilde         = Storage::disk('s3')->url($s3upload);
 
                         $bedrift->save();                        
                     }
                 else
                     {
-                        // Profilbildet ble ikke lastet opp
+                        // Profilbildet ble IKKE lastet opp
                         return "Feil ved opplasting av bilde til server :-(";
                     }
             }
