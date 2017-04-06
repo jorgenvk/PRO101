96607ace59a70de7a16080e67955e49cde7c3830
thyal <thomasdfrivold@gmail.com>
2017-04-06 10:29:32 +0200

    Satt startsiden til listefremvisning

--- gruppe35/routes/web.php
+++ gruppe35/routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'BedriftController@listBedrifter');
 
 
 Route::get('bedrift/ny', 'BedriftController@pageNyBedrift');
