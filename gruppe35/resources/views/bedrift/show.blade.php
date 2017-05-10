61ced2a76943b11514414e4201e99bc7f8f1f810
thyal <thomasdfrivold@gmail.com>
2017-05-10 16:48:41 +0200

    Lagd til kartfunksjon på bedriftside

--- gruppe35/resources/views/bedrift/show.blade.php
+++ gruppe35/resources/views/bedrift/show.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-md-10 col-md-offset-1">
       <div class="jumbotron">
         <h1> {{ $bedrift->Bedrift_navn }}</h1>
         <img src="{{ $bedrift->Bilde }}" class="bedriftbilde"/>
@@ -30,6 +30,16 @@
             </tr>
           </tbody>
         </table>
+        <div class="kart">
+          <h2>Finn {{ $bedrift->Bedrift_navn }} her!</h2>
+          <iframe
+            id="kartBedrift"
+            width="800"
+            height="450"
+            frameborder="0" style="border:0"
+            src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDywUZI16jpELxuAZ6VFnNtaGyElz-DQ-k&q={{$bedrift->Bedrift_navn}},{{$bedrift->Adresse}}" allowfullscreen>
+          </iframe>
+        </div>
       </div>
     </div>
   </div>
