dac706a133dfe79166470afcc577e73b6963a569
joakimvrangum <joakimvrangum@gmail.com>
2017-05-10 17:07:21 +0200

    La til Bootstrap JS for at lukking av dialogvindu skal fungere

--- gruppe35/resources/views/layout/master.blade.php
+++ gruppe35/resources/views/layout/master.blade.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" href="/css/style.css" type="text/css" />
 <!-- Link til jQuery Script for dynamisk oppdatering av adresse i kartet -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Link til Boostrap JS for "lukking av dialogbokser" -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 @yield('header')
 </head>
 <body>
