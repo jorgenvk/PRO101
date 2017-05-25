5581cfe1755bafff86d4aebffa286429d516618f
thyal <thomasdfrivold@gmail.com>
2017-05-25 14:09:37 +0200

    La til bildeopplastning på bedrift.show

--- gruppe35/resources/views/bedrift/show.blade.php
+++ gruppe35/resources/views/bedrift/show.blade.php
@@ -43,4 +43,37 @@
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Last opp ditt eget bilde her!</h1>
+      <hr>
+      <div class="col-sm-2 lastoppboks">
+
+      <form action="{{ url('bilder/upload') }}" method="POST" enctype="multipart/form-data" name="formBilderUpload" id="formBilderUpload">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="Bedrift_id" value="{{ $bedrift->id }}">
+        <label class="btn btn-info pull-right" for="fil" style="margin-left: 10px;">
+        <input id="fil" name="fil" type="file" style="display: none;"/>
+        <span class="glyphicon glyphicon-camera"></span>
+        </label>
+      </form>
+      </div>
+
+      @foreach ($bedrift->bilder()->orderBy('created_at', 'desc')->get() as $bilde)
+          <div class="col-sm-2 bildeboks">
+            <img src="{{ Storage::disk('s3')->url($bilde->bilde) }}" class="img-responsive">
+          </div>
+      @endforeach
+
+      </div>
+  </div><!-- ..row -->
+
+
+  <script>
+  $(function () {
+    $("#fil").on("change", function(){
+      $('form#formBilderUpload').submit();
+    });
+    });
+  </script>
 @endsection
