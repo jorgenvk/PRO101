f12715228ff6dda6e2c4d742edd7c3ad2540e264
thyal <thomasdfrivold@gmail.com>
2017-05-26 14:04:16 +0200

    Endret litt design

--- gruppe35/resources/views/bedrift/show.blade.php
+++ gruppe35/resources/views/bedrift/show.blade.php
@@ -3,46 +3,33 @@
 @section('tittel', "$bedrift->Bedrift_navn")
 
 @section('body')
-
-  <div class="row">
-    <div class="col-md-10 col-md-offset-1">
-      <div class="jumbotron">
-        <h1> {{ $bedrift->Bedrift_navn }}</h1>
-        <img src="{{ $bedrift->Bilde }}" class="bedriftbilde"/>
-        <p> {{ $bedrift->Beskrivelse }} </p>
-        <table class="table">
-          <thead>
-          <tr>
-            <th>Kategori:</th>
-            <th>Adresse:</th>
-            <th>Telefon:</th>
-            <th>Åpningstider:</th>
-            <th>Nettside:</th>
-          </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{{ $bedrift->kategori->Kategori_navn }}</td>
-              <td>{{ $bedrift->Adresse }}</td>
-              <td>{{ $bedrift->Telefon }}</td>
-              <td>{{ $bedrift->Åpningstider }}</td>
-              <td><a href="http://{{ $bedrift->Nettside }}">{{ $bedrift->Nettside }}</a></td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="kart">
-          <h2>Finn {{ $bedrift->Bedrift_navn }} her!</h2>
-          <iframe
-            id="kartBedrift"
-            width="800"
-            height="450"
-            frameborder="0" style="border:0"
-            src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDywUZI16jpELxuAZ6VFnNtaGyElz-DQ-k&q={{$bedrift->Bedrift_navn}},{{$bedrift->Adresse}}" allowfullscreen>
-          </iframe>
-        </div>
-      </div>
-    </div>
+<div class="row">
+  <div class="kart">
+    <iframe
+      id="kartBedrift"
+      height="300"
+      frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/search?key=AIzaSyDywUZI16jpELxuAZ6VFnNtaGyElz-DQ-k&q={{$bedrift->Bedrift_navn}},{{$bedrift->Adresse}}" allowfullscreen>
+    </iframe>
   </div>
+</div>
+<div class="bedrift col-md-10 col-md-offset-1">
+  <div class="row">
+        <h1 class="text-center"> {{ $bedrift->Bedrift_navn }}</h1>
+        <div class="col-md-6">
+            <img src="{{ $bedrift->Bilde }}" class="bedriftbilde"/>
+        </div>
+        <div class="col-md-6 info-text">
+          <ul>
+            <li>Kategori: {{ $bedrift->kategori->Kategori_navn }}</li>
+            <li>Adresse: {{ $bedrift->Adresse }}</li>
+            <li>Telefon: {{ $bedrift->Telefon }}</li>
+            <li>Åpningstider: {{ $bedrift->Åpningstider }}</li>
+            <li>Nettside: {{ $bedrift->Nettside }}</li>
+          </ul>
+        </div>
+  </div>
+          <p> {{ $bedrift->Beskrivelse }} </p>
   <div class="row">
     <div class="col-md-12">
       <h1>Last opp ditt eget bilde her!</h1>
@@ -98,14 +85,16 @@
 <div class="col-md-8 col-md-offset-2">
   <h1>Kommentarer</h1>
   @foreach($bedrift->kommentarer as $kommentar)
-    <div class="comment">
+    <div class="kommentar">
       <p><b>Navn:</b> {{ $kommentar->navn }}</p>
-      <p>Epost: {{ $kommentar->epost }}</p>
+      <p><b>Epost:</b> {{ $kommentar->epost }}</p>
       <p><b>Kommentar:</b> <br />{{ $kommentar->kommentar }}</p><br /><br />
     </div>
   @endforeach
 </div>
 </div>
+</div>
+
 
 
   <script>
