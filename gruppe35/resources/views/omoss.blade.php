4e2ffcdcd2f33d5fb33a9f3dca5fb4073b6bad5b
jorgenvk <karjor16@student.westerdals.no>
2017-05-28 21:08:17 +0200

    ny logo

--- gruppe35/resources/views/omoss.blade.php
+++ gruppe35/resources/views/omoss.blade.php
@@ -3,9 +3,11 @@
 @section('tittel', 'Om oss - Westfinder')
 
 <div class="bedrift col-md-10 col-md-offset-1">
-    <h1 class="text-center">Om oss</h1>
-    <p>Vi er gruppe 35!</p>
-
+    <img src="/bilder/logo.png" style="height:250px;">
+    <h1>Westfinder</h1>
+    <p>Westfinder er en applikasjon for studenter ved Westerdals, og tar utgangspunkt i campusene ved Fjerdingen og Vulkan for å gi informasjon om deres nærmiljøer.</p>
+    <h2>Teamet</h2>
+    <p>Applikasjonen er designet og utviklet som et studentprosjekt av Hans, Joakim, Sarah, Thomas og Jørgen.</p>
 </div>
 
 <style>
